51041730d2ebf545d73bc5c96acc42489f728ad3
Paweł Siemieniuk <pawel.siemieniuk@softwarehut.pl>
2016-12-20 16:23:28 +0100

    Christmas card

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,5 +1,62 @@
-var gebi = function(id) { 
-  return document.getElementById(id);
-};
+$(function() {
+  
+  function pathPrepare ($el) {
+    var lineLength = $el[0].getTotalLength();
+    $el.css("stroke-dasharray", lineLength);
+    $el.css("stroke-dashoffset", lineLength);
+  }
 
-console.log('witaj świecie');
+  var $elements = [
+    {item: $('path.have1'), time: 0.35},
+    {item: $('path.have2'), time: 0.1},
+    {item: $('path.have3'), time: 0.12},
+    {item: $('path.have4'), time: 0.25},
+    {item: $('path.have5'), time: 0.1},
+    {item: $('path.have6'), time: 0.11},
+
+    {item: $('path.color1'), time: 0.1 },
+    {item: $('path.color2'), time: 0.1 },
+    {item: $('path.color3'), time: 0.1 },
+    {item: $('path.color4'), time: 0.1 },
+    {item: $('path.color5'), time: 0.1 },
+    {item: $('path.color6'), time: 0.1 },
+    {item: $('path.color7'), time: 0.1 },
+    {item: $('path.color8'), time: 0.1 },
+    {item: $('path.color9'), time: 0.1 },
+    {item: $('path.color10'), time: 0.1 },
+    {item: $('path.color11'), time: 0.1 },
+    {item: $('path.color12'), time: 0.1 },
+    {item: $('path.color13'), time: 0.1 },
+    {item: $('path.color14'), time: 0.1 },
+    {item: $('path.color15'), time: 0.1 },
+    {item: $('path.color16'), time: 0.1 },
+    {item: $('path.color17'), time: 0.1 },
+    {item: $('path.color18'), time: 0.1 },
+    {item: $('path.color19'), time: 0.1 },
+    {item: $('path.color20'), time: 0.1 },
+    {item: $('path.color21'), time: 0.1 },
+    {item: $('path.color22'), time: 0.1 },
+
+    {item: $('path.christmas1'), time: 1.71 },
+    {item: $('path.christmas2'), time: 0.3 },
+    {item: $('path.christmas3'), time: 0.06 },
+    {item: $('path.christmas4'), time: 0.5 },
+    {item: $('path.christmas5'), time: 0.045 },
+    {item: $('path.christmas6'), time: 0.08 },
+  ];
+
+  // init controller
+  var controller = new ScrollMagic.Controller();
+  var tween = new TimelineMax();
+
+  for (var i = 0, len = $elements.length; i < len; i++) {
+    pathPrepare($elements[i].item);
+    tween.to($elements[i].item, $elements[i].time, {strokeDashoffset: 0, ease:Linear.easeNone});
+  }
+
+  // build scene
+  var scene = new ScrollMagic.Scene({triggerElement: "#trigger1"})
+    .setTween(tween)
+    .addTo(controller);
+
+});
--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,28 @@ var gulp = require('gulp'),
     runSequence = require('run-sequence'),
     sass = require('gulp-sass'),
     plumber = require('gulp-plumber'),
-    gutil = require('gulp-util');
+    gutil = require('gulp-util'),
+    concat = require('gulp-concat'),
+    minify = require('gulp-minify');
+
+
+var cssmin = require('gulp-cssmin');
+var jsmin = require('gulp-jsmin');
+var fontmin = require('gulp-fontmin');
+
+var jshint = require('gulp-jshint');
+var htmlhint = require("gulp-htmlhint");
+var csslint = require('gulp-csslint');
+var usemin = require('gulp-usemin');
+
+var watch = require('gulp-watch');
+var browserSync = require('browser-sync').create();
+
+var path = require('path');
+var minifyHtml = require('gulp-minify-html');
+
+var uglify = require('gulp-uglify');  
+var rename = require('gulp-rename');
 
 
 
@@ -36,6 +57,91 @@ gulp.task('browser-sync', function () {
         }
     })
 });
+
+gulp.task('scripts', function() {
+  return gulp.src('./app/**/*.js')
+    .pipe(concat('main.js'))
+    .pipe(gulp.dest('dist/scripts/'));
+});
+
+gulp.task('styles', function() {
+  return gulp.src('./app/**/*.css')
+    .pipe(concat('main.css'))
+    .pipe(gulp.dest('dist/styles/'));
+});
+
+gulp.task('html', function() {
+  return gulp.src('./app/*.html')
+    .pipe(concat('index.html'))
+    .pipe(gulp.dest('dist/'));
+});
+
+// script paths
+// var jsFiles = './app/styles/**/*.css',  
+//     jsDest = 'dist/styles';
+
+// gulp.task('styles', function() {  
+//     return gulp.src(jsFiles)
+//         .pipe(concat('styles.css'))
+//         .pipe(gulp.dest(jsDest))
+//         .pipe(rename('styles.min.css'))
+//         .pipe(cssmin())
+//         .pipe(gulp.dest(jsDest));
+// });
+
+
+// gulp.task('usemin', ['jshint', 'csshint'], function() {
+//   return gulp.src('./app/*.html')
+//     .pipe(usemin({
+//       css: [ cssmin() ],
+//       // bootstrap: [ cssmin() ],
+//       // ieOnly: [ cssmin() ],
+//       html: [ minifyHtml({ empty: true }) ],
+//       js: [ jsmin() ],
+//       // jquery: [ jsmin() ]
+//     }))
+//     .pipe(gulp.dest('dist/'));
+// });
+
+// gulp.task('jshint', function() {
+//     return gulp.src(['./app/scripts/*.js', '!js/*.min.js'])
+//         .pipe(jshint())
+//         .pipe(jshint.reporter('default'));
+// });
+
+// gulp.task('csshint', function() {
+//     return gulp.src('./app/styles/*.css')
+//         .pipe(csslint.reporter())
+//         .pipe(csslint.reporter('fail'));
+// });
+
+// gulp.task('htmlhint', function() {
+//     return gulp.src("./app/*.html")
+//         .pipe(htmlhint())
+//         .pipe(htmlhint.failReporter())
+// });
+
+
+gulp.task('fonts', function () {
+    return gulp.src(['./app/fonts/**/*.{ttf,woff,eof,svg}'])
+        .pipe(fontmin())
+        .pipe(gulp.dest('dist/fonts/'));
+});
+
+gulp.task('copy', function() {
+    /*
+    gulp.src(['css/fonts/**//*.{ttf,woff,eof,svg}'])
+        .pipe(gulp.dest('dist/css/fonts'));
+    */
+    gulp.src(['./app/images/**/*.png','./app/images/**/*.jpg','./app/images/**/*.gif','./app/images/**/*.jpeg', './app/images/**/*.svg'])
+        .pipe(gulp.dest('dist/images/'));
+    /*
+    gulp.src(['*.json'])
+        .pipe(gulp.dest('dist'));
+    */
+});
+
+gulp.task('build', ['scripts', 'styles', 'html', 'copy', 'fonts']);
 
 //default tasks
 //gulp.task('build', function (callback) {
